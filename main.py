6b76f98a250098c55be5abc65911e056e986375c
mandel94 <manueldeluzi@gmail.com>
2021-06-26 18:48:23 +0200

    Updates

--- main.py
+++ main.py
@@ -18,17 +18,10 @@ player_names = players[2]
 player_table.to_csv("test_player_table")
 
 # Retrieve dictionary with players' stats
-stats_dict_alt = retrieve_player_stats(player_soups=player_soups, 
+stats_dict = retrieve_player_stats(player_soups=player_soups, 
                                        player_names=player_names)
 
-stats_dict_alt['Forest Able']
+# Save test file
+stats_dict['Kareem Abdul-Jabbar'].to_csv("abdul_jabbar_career.csv")
 
-from cleany import DataCleaner
-from prepare_data import reformat_age_column
 
-cleaner = DataCleaner(stats_dict_alt['Forest Able'])
-
-cleaner.apply(reformat_age_column).input
-cleaner.input[["age"]]
-
-reformat_age_column(cleaner.input)
