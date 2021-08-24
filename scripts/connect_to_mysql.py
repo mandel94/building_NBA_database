29b4437b0a33b7e1c00beb5888dd9276bf51bf59
mandel94 <manueldeluzi@gmail.com>
2021-08-24 15:44:55 +0200

    Module for creating mysql connection complete

---
+++ scripts/connect_to_mysql.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 24 14:58:03 2021
+
+@author: mandel94
+"""
+
+'''This module will export a connection to MySQL server.
+   This connection will function as an interface for building our database  
+'''
+
+
+import os
+from getpass import getpass
+from mysql.connector import connect, Error
+
+
+def establish_connection(host_name, user_name, psswd):
+    ''''''  
+    try:
+        with connect(
+            host="localhost",
+            user=os.environ.get("MYSQLUSER"),
+            password=os.environ.get("MYSQLPSSWD"),
+        ) as connection:
+            print('Connection to MySQL Server established!')
+            return connection
+    except Error as e:
+        print("A connection to MySQL Server could not be established")
+        return e 
+
+
